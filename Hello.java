87f5ff4e4d18074d044a5b93674f556674a86dd8
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-05-19 22:03:35 +0500

    Data types or varaible in java

---
+++ Hello.java
@@ -0,0 +1,24 @@
+class Hello{
+    public static void main(String[] args) {
+        // System.out.println(3+7);
+        // System.out.print(3+12);
+        // System.out.println(3+6);
+        // short num=34;
+        // int num2=4;
+        // int res=num+num2;
+        int num1=9;
+        byte num2=127;
+        short sh=555;
+        long l=23233;
+
+        float f=5.8f;
+        double d=444.23;
+
+        char c='k';
+
+        boolean b=true;
+        System.out.println(num2);
+
+    }
+}
+
