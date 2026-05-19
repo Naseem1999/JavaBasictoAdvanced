2be89b27df4c34014c58cf45b887c37ad74ffaa3
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-05-19 22:22:31 +0500

    litterals in java

--- Hello.java
+++ Hello.java
@@ -1,23 +1,21 @@
 class Hello{
     public static void main(String[] args) {
-        // System.out.println(3+7);
-        // System.out.print(3+12);
-        // System.out.println(3+6);
-        // short num=34;
-        // int num2=4;
-        // int res=num+num2;
-        int num1=9;
-        byte num2=127;
-        short sh=555;
-        long l=23233;
+        //literals
+        // int num1=0x7E;
+        // int num1=10_00_00_000;
+        // double num1=45;
+        // double num1=12e10;
+        // boolean num1=1;
 
-        float f=5.8f;
-        double d=444.23;
+        // char c='a';
+        // System.out.println(c+1);
 
-        char c='k';
+        char c='a';
+        c++;
+        System.out.println(c);
 
-        boolean b=true;
-        System.out.println(num2);
+
+        //  System.out.println(num1);
 
     }
 }
