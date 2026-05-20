3b2f9cd133d788a0b53c48fe2d7db7157ea4ffdd
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-05-20 14:57:03 +0500

    relational operators in java

--- Hello.java
+++ Hello.java
@@ -1,28 +1,16 @@
 class Hello{
     public static void main(String[] args) {
         //Operators
+        int x=6;
+        int y=5;
 
-       int num1=7;
-       int num2=5;
+        // boolean result=x<y;
+        // boolean result=x>=y;
+        // boolean result=x!=y;
+        boolean result=x==y;
 
-    //   int result=num1+num2;
-    //   int result=num1*num2;
-    //   int result=num1-num2;
-    //   int result=num1/num2;
-        //  int result=num1%num2;
-        // num1=num1+2;
-        // num1 += 2;
-        // num1=num1+1;
-        // num1+=1;
-        // num1++;
-        // num1--;
 
-    // pre increment and postINCREMENT
-    // int res=++num1;  //8 first increment and then fetch value
-    //    int res=num1++; //7 first fetch the value then it will increment
-        
-
-    //    System.out.println(res);
+        System.out.println(result);
 
     }
 }
