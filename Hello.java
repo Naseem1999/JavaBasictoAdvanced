2fa4c6986046d9eec02bb6c308bfb269073e5922
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-05-21 12:45:16 +0500

    logical operators in java

--- Hello.java
+++ Hello.java
@@ -1,16 +1,16 @@
 class Hello{
     public static void main(String[] args) {
         //Operators
-        int x=6;
+        int x=7;
         int y=5;
+        int a=5;
+        int b=9;
 
-        // boolean result=x<y;
-        // boolean result=x>=y;
-        // boolean result=x!=y;
-        boolean result=x==y;
+        // boolean result=x>y || a >b;
+        boolean result=a >b;
+        
 
-
-        System.out.println(result);
+        System.out.println(!result);
 
     }
 }
