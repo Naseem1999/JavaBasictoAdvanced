9532c2f55800a8f2ede206e03121b4f9577e278e
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-05-21 17:40:50 +0500

    classes concept started

--- Hello.java
+++ Hello.java
@@ -1,28 +1,20 @@
+class Caculator{
+  int a;
+  public int add(int n1,int n2){
+    int r=n1+n2;
+    return r;
+  }
+}
+
+
 class Hello{
     public static void main(String[] args) {
-      //loops
-      for(int i=1;i<=5;i++){
-        System.out.println("Day " +i);
-        for(int j=1;j<=9;j++){
-          System.out.println("  " +(j+8) +" - " +(j+9));
-        }
-      }
-      // int i=1;
-      // while (i<4) {
-      //   System.out.println("Hi");
-      //   int j=1;
-      //   while(j<=3){
-      //     System.out.println("Hello");
-      //     j++;
-      //   }
-      //   i++;
-      // }
+      int num1=4;
+      int num2=5;
+    Caculator c1=new Caculator();
+    int result=c1.add(num1,num2);
+    System.out.println(result);
 
-      // do{
-      //    System.out.println("Hi");
-      //    i++;
-      // }
-      // while(i<0);
 
     }
 }
