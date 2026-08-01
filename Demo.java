74d8267a2019eb28329850d36e8eb80bdf5b2e35
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-08-01 16:49:54 +0500

    constructor in java

--- Demo.java
+++ Demo.java
@@ -8,6 +8,18 @@ class Humman{
  private int age;
  private String name;
 
+ 
+
+ public Humman(int age, String name) {
+    this.age = age;
+    this.name = name;
+}
+ public Humman(String name) {
+    this.name = name;
+}
+ public Humman() {
+    System.out.println("object created.....");
+}
  public int getAge(){
     return age;
  }
@@ -26,12 +38,19 @@ class Humman{
 public class Demo {
     public static void main(String[] args) {
     //Encapsulation 
-    Humman obj=new Humman();
-    obj.setName("Naseem");
-    obj.setAge(22,obj);
+    // Humman obj=new Humman();
+    // obj.setName("Naseem");
+    // obj.setAge(22,obj);
+    // System.out.println(obj.getName() + ":"+obj.getAge() );
+
+    //constructor
+  
+        Humman obj=new Humman(2,"ABC");
+        Humman obj1=new Humman();
+    // obj.setName("Naseem");
+    // obj.setAge(22,obj);
     System.out.println(obj.getName() + ":"+obj.getAge() );
-
-
+    System.out.println(obj1.getName() + ":"+obj1.getAge() );
 
 
     }
