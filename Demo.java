0abdbcbf13b69d622c9b445b9bf44de19132724d
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-08-26 00:17:19 +0500

    package in java

--- Demo.java
+++ Demo.java
@@ -1,18 +1,10 @@
-class Calc{
-    public int add(int n1,int n2){
-       return n1+n2;
-    }
-}
-class AdvCalc extends Calc{
-    // public int add(int n1,int n2){
-    //     return n1+n2+1;
-    // }
-}
+import tools.*;
 
 public class Demo {
     public static void main(String[] args)  {
-       AdvCalc obj=new AdvCalc();
-       int r1=obj.add(3, 4);
-       System.out.println(r1);
+      Calc c=new Calc();
+      AdvCalc adv=new AdvCalc();
+
+      System.out.println();
     }
 }
