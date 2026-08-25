b9fe6abc1e214dd0cc4036f96148c857bc74f277
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-08-25 23:37:52 +0500

    super and this keyword

--- Demo.java
+++ Demo.java
@@ -1,18 +1,32 @@
-
+class A{
+    int num=1;
+}
+class B extends A{
+    int num=2;
+    public int getValue(){
+        int num=3;
+        // return num;
+        // return this.num;
+        return super.num;
+    }
+}
 public class Demo {
     public static void main(String[] args)  {
-       VeryAdvCalc obj=new VeryAdvCalc();
+        B obj=new B();
+        // System.out.println(obj.num);
+        System.out.println(obj.getValue());
+    //    VeryAdvCalc obj=new VeryAdvCalc();
 
-       int r1=obj.add(3,3);
-       int r2=obj.sub(9,3);
-       int r3=obj.multi(9,3);
-       int r4=obj.div(9,3);
-       double r5=obj.power(2, 3);
+    //    int r1=obj.add(3,3);
+    //    int r2=obj.sub(9,3);
+    //    int r3=obj.multi(9,3);
+    //    int r4=obj.div(9,3);
+    //    double r5=obj.power(2, 3);
 
-       System.out.println(r1);
-       System.out.println(r2);
-       System.out.println(r3);
-       System.out.println(r4);
-       System.out.println(r5);
+    //    System.out.println(r1);
+    //    System.out.println(r2);
+    //    System.out.println(r3);
+    //    System.out.println(r4);
+    //    System.out.println(r5);
     }
 }
