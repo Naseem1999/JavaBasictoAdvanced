d0e36bf744be90831970e7b25dfb3133da100de9
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-08-29 00:02:59 +0500

    Polymorphism dynamic method dispatch

--- Demo.java
+++ Demo.java
@@ -1,17 +1,33 @@
 import other.*;
-
-class C extends A{
-  public void abc(){
-    System.out.println(marks);
-  }
+class A{
+public void show(){
+  System.out.println("In a Show");
 }
+}
+class B extends A{
+public void show(){
+  System.out.println("In a Show B");
+}
+}
+class C extends A{
+public void show(){
+  System.out.println("In a Show C");
+}
+}
+
 public class Demo {
     public static void main(String[] args)  {
+      // A obj=new B();
+      // obj.show();
+
       A obj=new A();
-      // System.out.println(obj.marks);
       obj.show();
 
-      B obj1=new B();
-      // System.out.println(obj1.marks);
+
+      obj=new B();
+      obj.show();
+
+      obj=new C();
+      obj.show();
     }
 }
