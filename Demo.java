f0d083d941161ceb6241619f64fed2a570cf09bf
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-08-28 23:49:49 +0500

    access modifiers in java

--- Demo.java
+++ Demo.java
@@ -1,10 +1,17 @@
-import tools.*;
+import other.*;
 
+class C extends A{
+  public void abc(){
+    System.out.println(marks);
+  }
+}
 public class Demo {
     public static void main(String[] args)  {
-      Calc c=new Calc();
-      AdvCalc adv=new AdvCalc();
+      A obj=new A();
+      // System.out.println(obj.marks);
+      obj.show();
 
-      System.out.println();
+      B obj1=new B();
+      // System.out.println(obj1.marks);
     }
 }
