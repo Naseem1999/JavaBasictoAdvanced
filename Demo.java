43461a239f4727272daafa7544a8239728dd8d9c
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-08-29 00:39:17 +0500

    object class,toString,equals,hashcode

--- Demo.java
+++ Demo.java
@@ -1,29 +1,32 @@
-//final variable,class,method
-
-class Calc{  //if you make class final then it should not be extended
-  public final void show(){ //if you not want to override the method use final keyword
-    System.out.println("In calc show");
+class Laptop{
+  String model;
+  int price;
+  @Override
+  public String toString() {
+    return "Laptop [model=" + model + ", price=" + price + "]";
   }
-  public void add(int a,int b){
-    System.out.println(a+b);
-  }
-}
 
-class AdvCalc extends Calc{  //
-  public void show(){
-    System.out.println("In calc show");
+  public boolean equals(Laptop that){
+    if(this.model.equals(that.model) && this.price==that.price){
+      return true;
+    }else{
+      return false;
+    }
   }
 }
 
 public class Demo {
     public static void main(String[] args)  {
-      // final int num=6;
-      // num=3;
-      // System.out.println(num);
+    Laptop obj=new Laptop();
+    obj.model="lenovo";
+    obj.price=2333;
+    Laptop obj1=new Laptop();
+    obj1.model="lenovo";
+    obj1.price=2333;
+    // System.out.println(obj);
 
-      Calc obj=new Calc();
-      obj.show();
-      obj.add(3, 5);
+    boolean res=obj.equals(obj1);
+    System.out.println(res);
 
     }
 }
