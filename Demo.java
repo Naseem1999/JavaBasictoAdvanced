c6bc5fe45aed7a64f34572e0d4091b1142d999db
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-08-29 01:08:05 +0500

    upcasting & downcasting and wrapper classes

--- Demo.java
+++ Demo.java
@@ -1,32 +1,46 @@
-class Laptop{
-  String model;
-  int price;
-  @Override
-  public String toString() {
-    return "Laptop [model=" + model + ", price=" + price + "]";
+class A{
+  public void show1(){
+    System.out.println("In A show");
   }
+}
 
-  public boolean equals(Laptop that){
-    if(this.model.equals(that.model) && this.price==that.price){
-      return true;
-    }else{
-      return false;
-    }
+class B extends A{
+  public void show2(){
+    System.out.println("In B show");
   }
 }
 
 public class Demo {
     public static void main(String[] args)  {
-    Laptop obj=new Laptop();
-    obj.model="lenovo";
-    obj.price=2333;
-    Laptop obj1=new Laptop();
-    obj1.model="lenovo";
-    obj1.price=2333;
-    // System.out.println(obj);
 
-    boolean res=obj.equals(obj1);
-    System.out.println(res);
+    //  double d=4.5;  
+    //  int a=(int)d;  //type casting
+
+    //  System.out.println(a);
+
+    // A obj =new A();
+    // obj.show1();
+      // A obj =(A)new B();  //upcasting
+      // obj.show1();
+
+      // B obj1=(B) obj;  //downcasting
+      // obj1.show2();
+
+
+      //wrapper class
+      // int num=7;
+      // Integer num1=4;
+      int num=7;
+      Integer num1=num;  //atoboxing
+
+      // int num2=num1.intValue();  //auto-unboxing
+      // int num2=num;//auto-unboxing
+      // System.out.println(num1);
+
+
+      String str="12";
+      Integer num2=Integer.parseInt(str);
+      System.out.println(num2*2);
 
     }
 }
